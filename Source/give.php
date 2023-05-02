e591639a15a9f5d3317bae3f0c170f55b387816a
mani <SayenaChat@gmail.com>
2023-05-02 23:16:11 +0330

    Fix bug: -data

--- Source/give.php
+++ Source/give.php
@@ -9,7 +9,7 @@ require "config.php";
 $channel = new SayenaChannel();
 $channel->SQLConnect();
 
-if (isset($_GET['access'])&&isset($_GET['secret'])&&isset($_GET['to'])&&isset($_GET['data'])&&!empty($_GET['access'])&&!empty($_GET['secret'])&&!empty($_GET['to'])&&!empty($_GET['data']))
+if (isset($_GET['access'])&&isset($_GET['secret'])&&isset($_GET['to'])&&!empty($_GET['access'])&&!empty($_GET['secret'])&&!empty($_GET['to']))
 {
     if ($channel->secret()&&$channel->access())
         echo $channel->Give();
