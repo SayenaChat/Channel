8f8454bee0ec9983a6f11352b1156654a4ba902e
mani <SayenaChat@gmail.com>
2023-05-07 17:40:18 +0330

    Delete Message

---
+++ Source/delete.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ *  Sayena Chat Communication Channel
+ *  (c) 2023 Sayena Team, Free Software MIT License
+ *  Sayena v0.2-beta
+ *
+ *  send/  Send a message to another
+ *  send/? secret=[secret-key]& access=[access-key]& to=[another-id] data=[encrypted-message]
+ *
+ */
+
+require "config.php";
+$channel = new SayenaChannel();
+$channel->SQLConnect();
+
+if ($channel->check('secret')&&$channel->check('access')&&$channel->check('id'))
+{
+    if ($channel->secret()&&$channel->access())
+        $channel->Delete();
+}
+?>
