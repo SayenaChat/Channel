1b52d038459ee9604f07530950e236091e5152ee
mani <SayenaChat@gmail.com>
2023-05-07 17:40:23 +0330

    Delete History

---
+++ Source/delete-history.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ *  Sayena Chat Communication Channel
+ *  (c) 2023 Sayena Team, Free Software MIT License
+ *  Sayena v0.2-beta
+ *
+ *  give/   Give all messages from another in channel
+ *  give/?  secret=[secret-key]& access=[access-key]& to=[another-id]
+ *
+ */
+
+require "config.php";
+$channel = new SayenaChannel();
+$channel->SQLConnect();
+
+if ($channel->check('secret')&&$channel->check('access')&&$channel->check('to'))
+{
+    if ($channel->secret()&&$channel->access())
+        echo $channel->DeleteHistory();
+}
+?>
